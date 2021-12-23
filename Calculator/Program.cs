141992bf63af398b5663cb20bcfd26a34094151f
Rikard <rikard@densloe.net>
2021-12-23 21:44:36 +0100

    xUnit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,77 +2,137 @@
 
 namespace Calculator
 {
-    class Program
+    public class Program
     {
-        static void Addition()
-        {
-            Console.WriteLine("Skriv in ditt först nummer: ");
-            double firstnum = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Skriv in ditt andra nummer: ");
-            double secondnum = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(firstnum + " + " + secondnum + " = " + (firstnum + secondnum));
-        }
+        //static void Input() {
+        //    Console.WriteLine("Skriv in ditt först nummer: ");
+        //    double firstnum = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine("Skriv in ditt andra nummer: ");
+        //    double secondnum = Convert.ToDouble(Console.ReadLine());
+        //}
+        //static void Addition()
+        //{
+        //    Console.WriteLine("Skriv in ditt först nummer: ");
+        //    double firstnum = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine("Skriv in ditt andra nummer: ");
+        //    double secondnum = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine(firstnum + " + " + secondnum + " = " + (firstnum + secondnum));
+        //}
 
-        static void Subtraction()
-        {
-            Console.WriteLine("Skriv in ditt först nummer: ");
-            double firstnum = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Skriv in ditt andra nummer: ");
-            double secondnum = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(firstnum + " + " + secondnum + " = " + (firstnum - secondnum));
-        }
+        //static void Subtraction()
+        //{
+        //    Console.WriteLine("Skriv in ditt först nummer: ");
+        //    double firstnum = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine("Skriv in ditt andra nummer: ");
+        //    double secondnum = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine(firstnum + " - " + secondnum + " = " + (firstnum - secondnum));
+        //}
 
-        static void Multiplication()
-        {
-            Console.WriteLine("Skriv in ditt först nummer: ");
-            double firstnum = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Skriv in ditt andra nummer: ");
-            double secondnum = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(firstnum + " * " + secondnum + " = " + (firstnum * secondnum));
-        }
+        //static void Multiplication()
+        //{
+        //    Console.WriteLine("Skriv in ditt först nummer: ");
+        //    double firstnum = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine("Skriv in ditt andra nummer: ");
+        //    double secondnum = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine(firstnum + " * " + secondnum + " = " + (firstnum * secondnum));
+        //}
 
-        static void Division()
-        {
-            Console.WriteLine("Skriv in ditt först nummer: ");
-            double firstnum = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Skriv in ditt andra nummer: ");
-            double secondnum = Convert.ToDouble(Console.ReadLine());
-            if (secondnum == 0)
-            {
-                Console.WriteLine("Du kan inte dividera med 0. ");
-            }
-            else {
-                Console.WriteLine(firstnum + " / " + secondnum + " = " + (firstnum / secondnum));
-            }
+        //static void Division()
+        //{
+        //    Console.WriteLine("Skriv in ditt först nummer: ");
+        //    double firstnum = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine("Skriv in ditt andra nummer: ");
+        //    double secondnum = Convert.ToDouble(Console.ReadLine());
+        //    if (secondnum == 0)
+        //    {
+        //        Console.WriteLine("Du kan inte dividera med 0. ");
+        //    }
+        //    else {
+        //        Console.WriteLine(firstnum + " / " + secondnum + " = " + (firstnum / secondnum));
+        //    }
             
+        //}
+        public static double Addition(double num1, double num2)
+        {
+            
+            return num1 + num2;
+            
+        }
+
+        public static double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double Addition(double[] numberArray)
+        {
+            double sum = 0;
+            for (int i = 0; i < numberArray.Length; i++){
+                sum += numberArray[i];
+            }
+                return sum;
+            
+        }
+
+        public static double Subtraction(double[] numberArray)
+        {
+            double difference = 0;
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                difference -= numberArray[i];
+            }
+            return difference;
+
+        }
+
+        public static double Multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Division(double num1, double num2)
+        {
+
+            if (num2 != 0)
+            {
+                return num1 / num2;
+
+            }
+            else
+            {
+                return 0;
+            }
+
         }
 
         static void Main(string[] args)
         {
+            double[] numberArray = { 1.6, 2.8, 0.0, 1.4 };
             Console.WriteLine("Välkommen till kalkylatorn. Skriv exit för att avsluta programmet.");
-            while (true) {
-                
+            while (true)
+            {
+
                 Console.WriteLine("Vill du [A]ddera, [S]ubtrahera, [M]ultiplicera eller [D]ividera? ");
                 string method = Console.ReadLine();
 
 
-                if (method.ToUpper() == "a".ToUpper())
+                if (method.ToUpper() == "A")
                 {
-                    Addition();
+                    Addition(1, 2);
                 }
-                else if (method.ToUpper() == "s".ToUpper())
+                else if (method.ToUpper() == "S")
                 {
-                    Subtraction();
+                    Subtraction(1, 2);
                 }
-                else if (method.ToUpper() == "m".ToUpper())
+                else if (method.ToUpper() == "M")
                 {
-                    Multiplication();
+                    Multiplication(1, 2);
                 }
-                else if (method.ToUpper() == "d".ToUpper())
+                else if (method.ToUpper() == "D")
                 {
-                    Division();
+                    Division(1, 2);
                 }
-                else if (method.ToUpper() == "exit".ToUpper())
+                else if (method.ToUpper() == "EXIT")
                 {
                     break;
                 }
